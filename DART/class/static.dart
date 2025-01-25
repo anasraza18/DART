164716fe2f77b30d3bab0bc294303c4a774ed7d5
anasraza18 <manasraza18@gmail.com>
2025-01-25 20:01:12 +0500

    New update

--- DART/class/static.dart
+++ DART/class/static.dart
@@ -3,10 +3,16 @@ void main(){
 //static sy direct class sy access kr skty hain
 person.name;
 print(person.name);
+
+person.display();
 }
 
 class person{
 
   static String? name = "Anas";
   int? age;
+
+  static display(){
+    print("ok");
+  }
 }
